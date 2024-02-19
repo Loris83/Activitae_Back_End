dfc5837850b1ce5993e90d3a9e0fe1cd31ffe8c7
erwann-lubrano <61875357+erwann-lubrano@users.noreply.github.com>
2024-02-19 12:20:25 +0100

    debut chat

---
+++ src/main/java/com/activitae/activitae/entities/Chat.java
@@ -0,0 +1,37 @@
+package com.activitae.activitae.entities;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "chats")
+public class Chat {
+	@Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+	
+	private List<Message> messages;
+	
+
+	public List<Message> getMessages() {
+		return messages;
+	}
+
+	public void setMessages(List<Message> messages) {
+		this.messages = messages;
+	}
+	
+	
+}
---
+++ src/main/java/com/activitae/activitae/entities/Message.java
@@ -0,0 +1,64 @@
+package com.activitae.activitae.entities;
+
+import java.util.Date;
+
+import com.fasterxml.jackson.annotation.JsonIdentityReference;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "messages")
+public class Message {
+	//@Id
+    //@GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+	
+    @Column(nullable = false)
+	private String content;
+	
+    @JsonIdentityReference(alwaysAsId = true)
+    @Column(nullable = false)
+	private User user;
+	
+    @Column(nullable = false)
+	private Date date;
+
+    
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+}
